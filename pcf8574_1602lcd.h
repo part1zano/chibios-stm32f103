99c39a419fc628e0e2c603a912ec7d75cdfee855
Maxim V Filimonov <part1zancheg@gmail.com>
2014-06-29 00:52:51 +0400

    pcf in progress: changed i2c address

--- pcf8574_1602lcd.h
+++ pcf8574_1602lcd.h
@@ -1,7 +1,7 @@
 #ifndef _PCM8574_1602LCD_
 #define _PCM8574_1602LCD_
 
-#define DEFAULT_ADDRESS 0b0100000 // address is changeable using A0..A3 pins
+#define DEFAULT_ADDRESS 0x27 // a0..a3 unsoldered -- 0x27; soldered: 0x20
 #define I2CD I2CD1 // change this
 
 #endif
